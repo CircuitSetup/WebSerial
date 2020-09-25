f338c07d4d9fc91a5966c979c7733e61f0d27033
jdeglavina <jdeg@sugarmanstudios.com>
2020-09-25 14:57:56 -0400

    added printf

--- src/WebSerial.cpp
+++ src/WebSerial.cpp
@@ -1,4 +1,4 @@
-#include <WebSerial.hpp>
+#include "WebSerial.hpp"
 
 WebSerialClass WebSerial;
 
@@ -41,6 +41,36 @@ void WebSerialClass::begin(AsyncWebServer *server, const char* url){
 
 void WebSerialClass::msgCallback(RecvMsgHandler _recv){
     _RecvFunc = _recv;
+}
+
+size_t WebSerialClass::printf(const char *format, ...)
+{
+    char loc_buf[64];
+    char * temp = loc_buf;
+    va_list arg;
+    va_list copy;
+    va_start(arg, format);
+    va_copy(copy, arg);
+    int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
+    va_end(copy);
+    if(len < 0) {
+        va_end(arg);
+        return 0;
+    };
+    if(len >= sizeof(loc_buf)){
+        temp = (char*) malloc(len+1);
+        if(temp == NULL) {
+            va_end(arg);
+            return 0;
+        }
+        len = vsnprintf(temp, len+1, format, arg);
+    }
+    va_end(arg);
+    len = write((uint8_t*)temp, len);
+    if(temp != loc_buf){
+        free(temp);
+    }
+    return len;
 }
 
 // Print
--- src/WebSerial.hpp
+++ src/WebSerial.hpp
@@ -3,6 +3,7 @@
 
 #include "Arduino.h"
 #include "stdlib_noniso.h"
+#include <functional>
 
 #if defined(ESP8266)
     #define HARDWARE "ESP8266"
@@ -22,7 +23,6 @@
 
 typedef std::function<void(uint8_t *data, size_t len)> RecvMsgHandler;
 
-
 class WebSerialClass{
 
 public:
@@ -30,47 +30,45 @@ public:
 
     void msgCallback(RecvMsgHandler _recv);
 
+    virtual size_t write(uint8_t); //= 0;
+    size_t write(const char *str)
+    {
+        if(str == NULL) {
+            return 0;
+        }
+        return write((const uint8_t *) str, strlen(str));
+    }
+    virtual size_t write(const uint8_t *buffer, size_t size);
+    size_t write(const char *buffer, size_t size)
+    {
+        return write((const uint8_t *) buffer, size);
+    }
+    
+
+    size_t printf(const char * format, ...)  __attribute__ ((format (printf, 2, 3)));
+
     // Print
-
     void print(String m = "");
-
     void print(const char *m);
-
     void print(char *m);
-
     void print(int m);
-
     void print(uint8_t m);
-
     void print(uint16_t m);
-
     void print(uint32_t m);
-
     void print(double m);
-
     void print(float m);
 
 
     // Print with New Line
-
     void println(String m = "");
-
     void println(const char *m);
-
     void println(char *m);
-
     void println(int m);
-
     void println(uint8_t m);
-
     void println(uint16_t m);
-
     void println(uint32_t m);
-
     void println(float m);
-
     void println(double m);
-
 
 private:
     AsyncWebServer *_server;
